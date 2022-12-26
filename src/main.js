b3148673efb8141cc4c4cc34510381e506d5a730
Wagner Costa <wagner.dev016@gmail.com>
2022-12-25 21:23:28 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const buttons = document.querySelectorAll('[data-tab-button]')
     const tabsContainer = document.querySelectorAll('[data-tab-id]')
+    const questions = document.querySelectorAll('[data-faq-question]')
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+
+    window.addEventListener('scroll', function() {
+
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+
+    })
+
+
+    // Seção de atrações, programação das abas.
     for (let i=0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
 
@@ -15,7 +33,23 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+
+    //seção FAQ accordion.
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta );
+    }
+
 })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header')
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header')
+    header.classList.remove('header--is-hidden')
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]')
@@ -32,3 +66,12 @@ function escondeTodasAbas() {
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
 }
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
